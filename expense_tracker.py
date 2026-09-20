33c9a85be7e072ffa1f3daa6ffeaac00104d1813
Galileo <150097868+GiloTV@users.noreply.github.com>
2026-09-19 19:09:59 -0600

    Create the expense_data function to get data for new expenses

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,3 +1,42 @@
+from datetime import datetime
+
+def expense_data():
+    date = datetime.today().strftime('%Y-%m-%d')
+    category = ''
+    while not category:
+        category = input("""
+            Insert the category of the expense
+            1.- Service
+            2.- Food
+            3.- Emergency
+            4.- Hobby
+        """)
+        match category:
+            case '1':
+                category = 'Service'
+            case '2':
+                category = 'Food'
+            case '3':
+                category = 'Emergency'
+            case '4':
+                category = 'Hobby' 
+            case _:
+                category = ''
+                print('No category selected. Try again')
+    description = input("Add a little description about the expense")
+
+    while True:
+        amount = input("Insert the amount of the expense")
+        try:
+            amount = int(amount)
+            break
+        except ValueError:
+            print("Invalid amount. Please type a number")
+
+    new_expense = (date, category, description, amount)
+    print(type(new_expense))
+    print("expense details", new_expense)
+
 while True:
     opt = input(f"""
     {'*' * 30}
@@ -12,7 +51,7 @@ while True:
     match opt:
         case '1':
             print("Add new expense:")
-            
+            expense_data()
         case '2':
             print("Modify an expense")
              
